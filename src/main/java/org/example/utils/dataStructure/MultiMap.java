3399f4d6c1a8477ca7e35583deee98dcc4bc6292
YunFy26 <207946431@qq.com>
2024-04-16 23:36:47 +0800

    Multi-value mapping interface

---
+++ src/main/java/org/example/utils/dataStructure/MultiMap.java
@@ -0,0 +1,145 @@
+package org.example.utils.dataStructure;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+import lombok.NonNull;
+
+/**
+ * 是否存在键值对
+ * 获得键值对
+ * 添加键值对
+ * 移除键值对
+ * 返回键、值、键值对的集合
+ * 遍历键值对（默认操作，对MultiMap中的每个键值对执行给定的操作）
+ * Map的长度
+ * Map是否为空
+ */
+
+/**
+ * 多重映射接口
+ *
+ * @param <K> 键类型
+ * @param <V> 值类型
+ */
+public interface MultiMap<K, V> {
+
+    /**
+     * 是否存在一个键值对（key, value）
+     * @param key
+     * @param value
+     * @return
+     */
+    boolean contains(K key, V value);
+
+    /**
+     * 是否存在一个键为key的键值对
+     * @param key
+     * @return
+     */
+    boolean containsKey(K key);
+
+    /**
+     * 是否存在一个值为value的键值对
+     * @param value
+     * @return
+     */
+    boolean containsValue(V value);
+
+    /**
+     * 获取键key对应的值集合
+     * @param key
+     * @return
+     */
+    Set<V> get(K key);
+
+    /**
+     * 存储键值对
+     * @param key
+     * @param value
+     */
+    boolean put(@NonNull K key, @NonNull V value);
+
+    /**
+     * 将多个值映射到键key
+     * @param key
+     * @param values
+     */
+    boolean putAll(@NonNull K key, @NonNull Collection<? extends V> values);
+
+    /**
+     * 将多重映射中的所有键值对存储到当前映射中
+     * @param multiMap
+     */
+    boolean putAll(@NonNull MultiMap<? extends K, ? extends V> multiMap);
+
+    /**
+     * 移除键值对
+     * @param key
+     * @param value
+     */
+    boolean remove(K key, V value);
+
+    /**
+     * 移除与键key关联的所有键值对
+     * @param key
+     */
+    boolean removeAll(K key);
+
+    /**
+     * 移除键key的一组键值对
+     * @param key
+     * @param values
+     */
+    boolean removeAll(K key, Collection<? extends V> values);
+
+    /**
+     * 返回映射中所有键的集合
+     * @return
+     */
+    Set<K> keySet();
+
+    /**
+     * 返回映射中所有值的集合
+     * @return
+     */
+    Collection<V> values();
+
+    /**
+     * 返回映射中所有键值对的集合
+     * @return
+     */
+    Set<Map.Entry<K, V>> entrySet();
+
+    /**
+     * {k, {v1, v2, v3}} => {k, v1}, {k, v2}, {k, v3}
+     *
+     */
+    default void forEach(@NonNull BiConsumer<K, V> action) {
+        entrySet().forEach(entry -> action.accept(entry.getKey(), entry.getValue()));
+    }
+
+    /**
+     * {k, {v1, v2, v3}} => {k， {v1, v2, v3}}
+     */
+    void forEachSet(@NonNull BiConsumer<K, Collection<V>> action);
+
+    /**
+     * 清空映射
+     */
+    void clear();
+
+    /**
+     * 返回映射的大小
+     * @return size of the map
+     */
+    int size();
+
+    /**
+     * 判断映射是否为空
+     * @return true
+     */
+    boolean isEmpty();
+}
