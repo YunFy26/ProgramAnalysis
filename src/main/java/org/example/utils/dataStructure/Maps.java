d8d4a8b4996b938b7c2a4e622e1920968f26b464
YunFy26 <207946431@qq.com>
2024-04-17 17:41:12 +0800

    MapsFactory

--- src/main/java/org/example/utils/dataStructure/Maps.java
+++ src/main/java/org/example/utils/dataStructure/Maps.java
@@ -1,26 +1,50 @@
-package org.example.utils.factory;
-
-import org.example.utils.dataStructure.MultiMap;
+package org.example.utils.dataStructure;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public final class MapFactory {
-    private MapFactory(){}
+public final class Maps {
 
+    private Maps(){}
+
+    /**
+     * 创建一个新的HashMap
+     * @return
+     * @param <K>
+     * @param <V>
+     */
     public static <K, V> Map<K, V> newHashMap() {
         return new HashMap<>();
     }
 
+    /**
+     * 创建一个指定长度的HashMap
+     * @param size 长度
+     * @return
+     * @param <K>
+     * @param <V>
+     */
     public static <K, V> Map<K, V> newHashMap(int size) {
         return new HashMap<>(size);
     }
 
+    /**
+     * 创建一个新的LinkedHashMap
+     * @return
+     * @param <K>
+     * @param <V>
+     */
     public static <K, V> Map<K, V> newLinkedMap() {
         return new LinkedHashMap<>();
     }
 
+    /**
+     * 创建一个新的MultiMap
+     * @return
+     * @param <K>
+     * @param <V>
+     */
     public static <K, V> MultiMap<K, V> newMultiMap() {
         return new HashMap<>();
     }
