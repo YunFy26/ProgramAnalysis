ac7792a3584658fd0fde1db313d4f6a4c3dfd2e6
YunFy26 <207946431@qq.com>
2024-04-18 21:34:04 +0800

    Interface: SetEx expands java.util.Set

---
+++ src/main/java/org/example/utils/dataStructure/SetEx.java
@@ -0,0 +1,21 @@
+package org.example.utils.dataStructure;
+
+
+import org.example.utils.Copyable;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * 扩展Set接口
+ * @param <E> Set中元素的类型
+ */
+public interface SetEx<E> extends Set<E>, Copyable<SetEx<E>> {
+    /**
+     * 添加集合c中的所有元素，并返回（调用了此方法的集合）和（目标集合c）的差集
+     * @param c 目标集合c
+     * @return （调用了此方法的集合）和（目标集合c）的差集
+     */
+    SetEx<E> addAllReturnDiff(Collection<? extends E> c);
+
+}
