60158868060df7f78ec12d36d9a0931785771d69
YunFy26 <207946431@qq.com>
2024-04-20 22:08:04 +0800

    LargeSet

--- src/main/java/org/example/utils/collections/HybridHashSet.java
+++ src/main/java/org/example/utils/collections/HybridHashSet.java
@@ -1,9 +1,29 @@
 package org.example.utils.collections;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 基于HashSet的HybridSet，用于存储规模较大的数据
  * @param <E>
  */
 public final class HybridHashSet<E> extends AbstractHybridSet<E>{
 
+    public HybridHashSet() {
+    }
+
+    public HybridHashSet(Collection<E> c) {
+        super(c);
+    }
+
+    @Override
+    protected Set<E> newLargeSet(int initCapacity) {
+        return new HashSet<E>(initCapacity);
+    }
+
+    @Override
+    protected SetEx<E> newSet() {
+        return new HybridHashSet<>();
+    }
 }
