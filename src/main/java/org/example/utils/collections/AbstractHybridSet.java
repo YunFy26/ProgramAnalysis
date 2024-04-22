4a79e303887133a748ed8f93d32ae0a1d64a7202
YunFy26 <207946431@qq.com>
2024-04-22 22:08:49 +0800

    update

--- src/main/java/org/example/utils/collections/AbstractHybridSet.java
+++ src/main/java/org/example/utils/collections/AbstractHybridSet.java
@@ -4,6 +4,7 @@ import javax.annotation.Nonnull;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -18,6 +19,12 @@ public abstract class AbstractHybridSet<E> extends AbstractSetEx<E> implements S
     private static final String NULL_MESSAGE = "HybridSet does not permit null element";
 
     private static final int THRESHOLD = 8;
+
+    private Set<E> set;
+
+    private E singleton;
+
+    private boolean isLargeSet = false;
 
     protected AbstractHybridSet() {
     }
@@ -36,83 +43,88 @@ public abstract class AbstractHybridSet<E> extends AbstractSetEx<E> implements S
 
     protected abstract Set<E> newLargeSet(int initCapacity);
 
-
-
-    @Override
-    @Nonnull
-    public Object[] toArray() {
-        return super.toArray();
+    protected void upgradeToSmallSet(){
+        set = newSmallSet();
+        if (singleton != null){
+            set.add(singleton);
+            singleton = null;
+        }
     }
 
-    @Override
-    @Nonnull
-    public <T> T[] toArray(@Nonnull T[] a) {
-        return super.toArray(a);
+    protected void upgradeToLargeSet(int initCapacity){
+        assert !isLargeSet;
+        Set<E> origin = set;
+        set = newLargeSet(initCapacity);
+        if (singleton != null){
+            set.add(singleton);
+            singleton = null;
+        }
+        if (origin != null){
+            set.addAll(origin);
+        }
+        isLargeSet = true;
     }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
-    }
-
-    @Override
-    public boolean retainAll(@Nonnull Collection<?> c) {
-        return super.retainAll(c);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return super.removeAll(c);
-    }
-
-    @Override
-    @Nonnull
-    public Iterator<E> iterator() {
-        return null;
-    }
-
+    /**
+     * 如果singleton不为空，则说明当前集合元素只有一个，再增加一个元素需要转变为set
+     * 如果singleton为空，set不为空，则说明当前集合元素大于一个，如果不是大集合，且元素个数+1将会大于阈值，需要转变为大集合再增加
+     * 如果singleton为空，set为空，则说明当前集合元素为0，直接将元素赋值给singleton
+     * @param e element whose presence in this collection is to be ensured
+     * @return true if element is added successfully
+     */
     @Override
     public boolean add(E e) {
-        return super.add(e);
+        Objects.requireNonNull(e, NULL_MESSAGE);
+        if (singleton != null){
+            if (singleton.equals(e)){
+                return false;
+            }
+            singleton = null;
+            upgradeToSmallSet();
+        }
+        if (set != null){
+            if(!isLargeSet && set.size() + 1 > getThreshold()) {
+                upgradeToLargeSet(getThreshold() * 2);
+            }
+            set.add(e);
+        }
+        singleton = e;
+        return true;
     }
 
     @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return super.addAll(c);
-    }
-
-    @Override
-    public void clear() {
-        super.clear();
+    public boolean addAll(@Nonnull Collection<? extends E> c){
+        if (!isLargeSet){
+            int cSize = c.size();
+            if (cSize == 0) return false;
+            int newSize = cSize + size();
+            int threshold = getThreshold();
+            if (set == null){
+                if (newSize == 1){
+                    assert singleton == null;
+                    E e = CollectionsUtils.getOne(c);
+                    singleton = Objects.requireNonNull(e, NULL_MESSAGE);
+                    return true;
+                } else if (newSize <= threshold) {
+                    upgradeToSmallSet();
+                }else {
+                    upgradeToLargeSet(newSize);
+                }
+            }else if(newSize > threshold){
+                upgradeToLargeSet(newSize);
+            }
+            return set.addAll(c);
+        }
     }
 
     @Override
     public int size() {
+        if (singleton != null){
+            return 1;
+        }
+        if (set != null){
+            return set.size();
+        }
         return 0;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return super.containsAll(c);
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return super.contains(o);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return super.isEmpty();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return super.remove(o);
     }
 }
