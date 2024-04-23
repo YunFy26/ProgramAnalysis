7551d8b25b7ee892b774ceb7170374cd18826b15
YunFy26 <207946431@qq.com>
2024-04-23 20:19:06 +0800

    Update

--- src/main/java/org/example/utils/collections/AbstractHybridSet.java
+++ src/main/java/org/example/utils/collections/AbstractHybridSet.java
@@ -2,10 +2,7 @@ package org.example.utils.collections;
 
 import javax.annotation.Nonnull;
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -113,7 +110,125 @@ public abstract class AbstractHybridSet<E> extends AbstractSetEx<E> implements S
             }else if(newSize > threshold){
                 upgradeToLargeSet(newSize);
             }
-            return set.addAll(c);
+        }
+        if (!(c instanceof AbstractHybridSet<? extends E>)){
+            c.forEach(e -> Objects.requireNonNull(e, NULL_MESSAGE));
+        }
+        return set.addAll(c);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (singleton != null){
+            return singleton.equals(o);
+        }
+        if (set != null){
+            return set.contains(o);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(@Nonnull Collection<?> c) {
+        for (Object o : c){
+            if (!contains(o)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (singleton != null){
+            if (singleton.equals(o)){
+                singleton = null;
+                return true;
+            }
+            return false;
+        }
+        if (set != null){
+            return set.remove(o);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        if (size() > c.size()){
+            for (Object o : c)
+                modified |= remove(o);
+        }else {
+            for (Iterator<E> it = iterator(); it.hasNext();){
+                if (c.contains(it.next())){
+                    it.remove();
+                    modified = true;
+                }
+            }
+
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(@Nonnull Collection<?> c) {
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        for (Iterator<E> it = iterator(); it.hasNext();){
+            if (!c.contains(it.next())){
+                it.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public Iterator<E> iterator(){
+        if (singleton != null){
+            return new Iterator<>() {
+                // 声明一个类的成员变量时，如果没有初始化，那么会自动初始化为变量类型的默认值
+                boolean done;
+
+                @Override
+                public boolean hasNext() {
+                    return !done;
+                }
+
+                @Override
+                public E next() {
+                    if (done){
+                        throw new NoSuchElementException();
+                    }
+                    done = true;
+                    return singleton;
+                }
+
+                @Override
+                public void remove() {
+                    if (done && singleton != null){
+                        singleton = null;
+                    }else {
+                        throw new IllegalStateException();
+                    }
+                }
+            };
+        }
+        if (set != null){
+            return set.iterator();
+        }
+        return Collections.emptyIterator();
+    }
+
+    @Override
+    public void clear() {
+        if (singleton != null){
+            singleton = null;
+        }
+        if (set != null){
+            set.clear();
         }
     }
 
@@ -126,5 +241,65 @@ public abstract class AbstractHybridSet<E> extends AbstractSetEx<E> implements S
             return set.size();
         }
         return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (singleton != null){
+            return false;
+        }
+        if (set != null){
+            return set.isEmpty();
+        }
+        return true;
+    }
+
+    @Override
+    @Nonnull
+    public Object[] toArray() {
+        if (singleton != null){
+            return new Object[]{singleton};
+        }
+        if (set != null){
+            return set.toArray();
+        }
+        return new Object[0];
+    }
+
+    // TODO: public <T> T[] toArray(T[] a)
+//    @Override
+//    @Nonnull
+//    public <T> T[] toArray(@Nonnull T[] a) {
+//        if (singleton != null){
+//
+//        }
+//    }
+
+    @Override
+    public int hashCode(){
+        if (singleton != null){
+            return singleton.hashCode();
+        }
+        if (set != null){
+            return set.hashCode();
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == this){
+            return true;
+        }
+        if (!(o instanceof Set<?> set)){
+            return false;
+        }
+        if (size() != set.size()){
+            return false;
+        }
+        if (hashCode() != set.hashCode()){
+            return false;
+        }
+        return containsAll(set);
     }
 }
