104fa63750c5c9a76b50f72f7a9d5bda03e1d538
YunFy26 <207946431@qq.com>
2024-04-24 21:00:21 +0800

    Update

--- src/main/java/org/example/utils/collections/MapSetMultiMap.java
+++ src/main/java/org/example/utils/collections/MapSetMultiMap.java
@@ -19,6 +19,7 @@ public class MapSetMultiMap<K, V> extends AbstractMultiMap<K, V> implements Seri
 
     private final SSupplier<Set<V>> setFactory;
 
+    // 表示Map中的value的总数
     private int size = 0;
 
 
@@ -48,60 +49,160 @@ public class MapSetMultiMap<K, V> extends AbstractMultiMap<K, V> implements Seri
                 Collections.unmodifiableSet(values);
     }
 
+    private Set<V> getOrCreateSet(@Nonnull K key){
+        // 如果没有这个key，则创建一个Set<V>实例
+        return map.computeIfAbsent(key, __ -> setFactory.get());
+    }
+
     @Override
     public boolean put(@Nonnull K key, @Nonnull V value) {
         Objects.requireNonNull(key, NULL_KEY);
         Objects.requireNonNull(value, NULL_VALUE);
-
+        if (getOrCreateSet(key).add(value)){
+            ++size;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean putAll(@Nonnull K key, @Nonnull Collection<? extends V> values) {
+        Objects.requireNonNull(key, NULL_KEY);
+        Objects.requireNonNull(values);
+        if (!values.isEmpty()){
+            Set<V> set = getOrCreateSet(key);
+            int diff = 0;
+            for (V v : values){
+                if (set.add(Objects.requireNonNull(v, NULL_VALUE))){
+                    ++diff;
+                }
+            }
+            if (diff > 0){
+                size += diff;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean putAll(@Nonnull MultiMap<? extends K, ? extends V> multiMap) {
-        return false;
+        Objects.requireNonNull(multiMap);
+        boolean[] modified = {false};
+        multiMap.forEachSet((key, values) -> modified[0] |= putAll(key, values));
+        return modified[0];
     }
 
     @Override
     public boolean remove(K key, V value) {
+        Collection<V> values = map.get(key);
+        if (values != null && values.remove(value)){
+            if (values.isEmpty()){
+                map.remove(key);
+            }
+            --size;
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean removeAll(K key) {
+        Set<V> values = map.remove(key);
+        if (values != null){
+            size -= values.size();
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean removeAll(K key, Collection<? extends V> values) {
+        Collection<V> haveValues = map.get(key);
+        if (haveValues != null){
+            int currSize = haveValues.size();
+            haveValues.removeAll(values);
+            int diff = currSize - haveValues.size();
+            if (diff > 0){
+                size -= diff;
+                if (haveValues.isEmpty()){
+                    map.remove(key);
+                }
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Set<K> keySet() {
-        return Set.of();
+        return Collections.unmodifiableSet(map.keySet());
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clear() {
+        map.clear();
+        size = 0;
+    }
 
+    @Override
+    public int hashCode(){
+        return map.hashCode();
     }
 
     @Override
     protected Iterator<Map.Entry<K, V>> entryIterator() {
-        return null;
+        return new EntryIterator();
+    }
+
+    private final class EntryIterator implements Iterator<Map.Entry<K, V>>{
+
+        private final Iterator<Map.Entry<K, Set<V>>> mapIt;
+
+        private Iterator<V> valueIt;
+
+        private K currKey;
+
+        private EntryIterator() {
+            this.mapIt = map.entrySet().iterator();
+            if (mapIt.hasNext()){
+                // var 声明局部变量
+                var entry = mapIt.next();
+                currKey = entry.getKey();
+                valueIt = entry.getValue().iterator();
+            }else {
+                valueIt = Collections.emptyIterator();
+            }
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return valueIt.hasNext() || mapIt.hasNext();
+        }
+
+        @Override
+        public Map.Entry<K, V> next() {
+            if(valueIt.hasNext()){
+                return new ImmutableMapEntry<>(currKey, valueIt.next());
+            }else if (mapIt.hasNext()){
+                var entry = mapIt.next();
+                currKey = entry.getKey();
+                valueIt = entry.getValue().iterator();
+                return new ImmutableMapEntry<>(currKey, valueIt.next());
+            }else {
+                throw new NoSuchElementException();
+            }
+        }
     }
 
     @Override
     public void forEachSet(@Nonnull BiConsumer<K, Collection<V>> action) {
-
+        map.forEach(action);
     }
 }
